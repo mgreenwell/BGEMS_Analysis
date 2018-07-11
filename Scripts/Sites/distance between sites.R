5c14f2f48040ecef6157dcd39485f7d2dd2ee08b
mgreenwell <matthew.greenwell@pgr.reading.ac.uk>
2018-07-11 10:17:45 +0100

    ANNOTATE script and tidy code

--- Scripts/Sites/distance between sites.R
+++ Scripts/Sites/distance between sites.R
@@ -1,49 +1,128 @@
+# =========================== Packages required =============================== 
+
+
 library(raster)
 library(tidyverse)
 
 
-locations <- read.csv("C:\\Users\\dp005352\\Dropbox\\PhD\\BGEMS\\site_locations\\european_coordinates.csv", header = T)
+# ============================= Load in data ==================================
+
+
+# european_coordinates.csv contains all site locatiosn for BGEMS across Europe
+
+locations <- read.csv("Data/Sites/european_coordinates.csv", header = T)
+
+
+# ============================== Format data ==================================
+
+
+# In locations columns are ordered latitude then longitude
+# R requires columns to be the other way around i.e. long (x) then lat (y)
 
 coordinates <- mutate(locations, longitude1 = longitude, latitude1 = latitude)
+
+
+# Remove orignal lat long columns
+
 coordinates <- subset(coordinates, select=-c(longitude, latitude))
+
+
+# Rename lat1 and long1
+# Remove all sites not in England
+# Remove Warton Cragg site as not used in analysis
+
 coordinates <- coordinates %>%
   rename(latitude = latitude1, longitude = longitude1) %>%
   filter (country == "England", name != "Warton Crag LNR")
 
 
-coordinates <- SpatialPointsDataFrame(coordinates[,5:6], coordinates, proj4string = CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"))
+# Turn coordinates into spatial points dataframe
+# coordinates gets a spatial point data frame using columns 5 (long) and 6 (lat)
+# of the dataframe coordinates
+# proj4string = CRS(etc) tells R which coordinate reference system to use
+  # Unlikely to plot without information
+  # Using WGS84 CRS
+  # Code to ut in CRS() available at:
+  # http://spatialreference.org/ref/epsg/wgs-84/
+  # http://spatialreference.org/ref/epsg/wgs-84/proj4/
+
+coordinates <-
+  SpatialPointsDataFrame(
+    coordinates[, 5:6],
+    coordinates,
+    proj4string = CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
+  )
+
+
+# Check whether working
+# Plot shows locations of points
 
 plot(coordinates)
 
-site_distances <- pointDistance(coordinates, coordinates, allpairs = T, lonlat=TRUE)
+
+# Create matrix of distances (meters) between pairs of sites
+
+site_distances <-
+  pointDistance(coordinates,
+                coordinates,
+                allpairs = T,
+                lonlat = TRUE)
+
+
+# Convert into a dataframe
+
 site_distances <- as.data.frame(site_distances)
+
+
+# Add row names and column names to matrix
 colnames(site_distances) <- c(as.character(coordinates$code))
 rownames(site_distances) <- c(as.character(coordinates$code))
-site_distances
+
+
+# Write matrix to a csv
+
 #write.csv(site_distances, "Outputs/site_distance_matrix.csv")
-  select(site_1,everything())
-head(site_distances)
+ 
+
+# Convert row names into the 1st column of the dataframe
 
 site_distances <- rownames_to_column(site_distances, var = "site_1")
 
+
+# Gather dataframe together so that instead of matrix, dataframe is long list
+# Reduce to three rows, site 1, site 2 and distance between sites in meters
+
 site_distances <- site_distances %>% 
   gather(site_2, distance, LC:WW)
-site_distances
+
+
+# Create new column of distances in KM and remove distances in M
 site_distances <- site_distances %>% 
   mutate(distance_km = distance / 1000) 
 site_distances <- subset(site_distances, select=-c(distance))
 
-arrange(site_distances, site_1, site_2)
+
+# Create list of site names
 
 site_list <- unique(site_distances$site_1)
 
+
+# Create empty dataframe
+
 site_distances_arranged <- NULL
-is.character(site_distances$site_1)
+
+
+# For loop rearranges dataframe so that the distances are ordered for each site
+# i.e. arranged highest to lowest distances between LC and all other sites
+# followed by highest to lowest distances between AU and all other sites
+
 for (i in site_list){
   df <- site_distances %>% 
     filter(site_1 == i) %>%
     arrange(distance_km)
   site_distances_arranged = rbind(site_distances_arranged, df)
 }
+
+
 site_distances_arranged
 
